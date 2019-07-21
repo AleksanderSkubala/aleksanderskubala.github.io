fad597be1dc35d89087f765d62179241fcf3a03b
buddy <buddy@buddy.works>
2019-07-21 15:53:06 +0000

    KPT deploy nr: 6

---
+++ kpt/src/App.vue
@@ -0,0 +1,85 @@
+<template>
+<div>
+  <transition name="fade">
+    <div class="ui container" v-if="step === 0">
+      <AppHeader @newK="newK"/>
+      <main class="ui main text container">
+        <List  @createFrom="createFrom"/>
+      </main>
+      <p id="footer">Created with <i>&#10084;</i> by Aleksander Skubała </br> &#x24D2; Copyright - All rights reserved.</p>
+    </div>
+  </transition>
+  <transition name="slider">
+    <FormView v-if="step === 1" @back="back" :konspektData="konspektData"/>
+  </transition>
+</div>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue';
+import List from '@/components/List.vue';
+import FormView from '@/components/FormView.vue';
+
+export default {
+  name: 'app',
+  components: {
+    AppHeader,
+    List,
+    FormView,
+  },
+  data() {
+    return {
+      step: 0,
+      loading: false,
+      konspektData: '',
+    };
+  },
+  methods: {
+    newK() {
+      this.konspektData = '';
+      this.step = 1;
+    },
+    back() {
+      // this.step = 0;
+      location.href = location.href;
+    },
+    createFrom(item) {
+      this.konspektData = item;
+      this.step = 1;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+
+.main.container {
+    margin-top: 5em !important;
+}
+
+.slider-enter-active, .slider-leave-active {
+  transition: opacity 0.7s ease;
+}
+.slider-enter, .slider-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active, .fade-leave-active {
+   transition: opacity 0.5s ease;
+}
+.fade-enter, .fade-leave-to {
+   opacity: 0;
+}
+
+#footer {
+    font-size: .99em;
+    text-align: center;
+    margin-top: 2em;
+
+    i {
+        color: #E53A40;
+        font-style: normal;
+    }
+}
+
+</style>
---
+++ kpt/src/components/FormView.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="ui container" id="container">
+        <!--<header class="ui header">
+            <FormHeader @back="$emit('back')" @newK="newK()"/>
+        </header>--> <!--It's merged with FormMain-->
+        <main class="ui main text container" >
+            <FormMain @back="$emit('back')" :konspektData="konspektData"/>
+            <p id="footer">Created with <i>&#10084;</i> by Aleksander Skubała </br> (I know that this app is badly built, but I love it ;D) </br> &#x24D2; Copyright - All rights reserved.</p>
+        </main>
+    </div>
+</template>
+
+<script>
+import FormMain from '@/components/FormComponents/FormMain.vue';
+
+export default {
+    name: "FormView",
+    components: {
+        FormMain,
+    },
+    data() {
+        return {
+            data: '',
+        }
+    },
+    props: {
+        konspektData: {
+            type: String,
+            required: false,
+        },
+    },
+    methods: {
+        newK() {
+            this.data = 'Hei';
+            alert(this.data);
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+#container{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+}
+
+.main.container{
+    margin: auto auto 3em;
+}
+
+#footer {
+    font-size: .9em;
+    text-align: center;
+    margin-top: 2em;
+
+    i {
+        color: #E53A40;
+        font-style: normal;
+    }
+}
+
+
+@media only screen and (max-width: 764px){
+    .ui.main.container{
+        margin: 6em 1em 3em !important;
+    }
+}
+
+@media only screen and (max-width: 575px){
+    .ui.main.text.container{
+        margin: 4em 0 3em !important;
+    }
+
+    .ui.container {
+        margin: 0 !important;
+    }
+}
+
+</style>
