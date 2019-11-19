1ef22fc14af0d2dfb2589578f17a1ee4d5cb2ce3
Aleksander Skubała <35501014+AleksanderSkubala@users.noreply.github.com>
2019-11-19 21:27:58 +0100

    add first version of MyCRM

---
+++ mycrm/precache-manifest.5c751aae897b6eadd9e14a980fc3c7ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73756c1edff070aa0b3e0d06271b391d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d338643b883bce75c86",
+    "url": "/static/css/main.e8bfc880.chunk.css"
+  },
+  {
+    "revision": "6ff24025f2c83fe0fd3c",
+    "url": "/static/js/2.3ce4f0bd.chunk.js"
+  },
+  {
+    "revision": "7d338643b883bce75c86",
+    "url": "/static/js/main.8dc95e64.chunk.js"
+  },
+  {
+    "revision": "edce8352e1548cfe2f75",
+    "url": "/static/js/runtime-main.7bdc8605.js"
+  }
+]);
